e765213b36a345a9962f8dd98651c90003bf709a
shaya99stern-hash <shaya99stern@gmail.com>
2026-05-04 18:30:01 -0400

    Make service worker update immediately

--- sw.js
+++ sw.js
@@ -1,22 +1,26 @@
-const CACHE_NAME = "sns-crm-v3";
+const CACHE_NAME = "sns-crm-v6";
 const ASSETS = [
   "./",
   "./index.html",
-  "./styles.css",
-  "./app.js",
+  "./styles.css?v=20260504-2",
+  "./app.js?v=20260504-2",
   "./manifest.webmanifest",
   "./assets/icon.svg",
 ];
 
 self.addEventListener("install", (event) => {
+  self.skipWaiting();
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))),
-    ),
+    Promise.all([
+      self.clients.claim(),
+      caches.keys().then((keys) =>
+        Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))),
+      ),
+    ]),
   );
 });
 
@@ -25,8 +29,12 @@ self.addEventListener("fetch", (event) => {
   if (new URL(event.request.url).origin !== self.location.origin) return;
 
   event.respondWith(
-    caches.match(event.request).then((cached) => {
-      return cached ?? fetch(event.request).catch(() => caches.match("./index.html"));
-    }),
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request).then((cached) => cached ?? caches.match("./index.html"))),
   );
 });
